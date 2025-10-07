3b4075adbc50fbc7d3a873d2920126e43396d2ac
bagil01 <bagil2203@gmail.com>
2025-10-07 14:14:06 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Bem-vindo à Plataforma de Alfabetização 👩‍🏫📚</h1>
+      <p>Projeto inicial rodando com React + Vite!</p>
+    </div>
+  );
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,1 @@
-function App() {
-  return (
-    <div>
-      <h1>Olá, mundo!</h1>
-      <p>Essa é uma página em React usando JSX.</p>
-    </div>
-  );
-}
+
